6bf145cbafe20774b33aa9aebadfad807d41e5b8
Artem Shebanov <crazymeat.008@gmail.com>
2023-03-31 16:10:41 +0300

    Project ready

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,22 @@
 import * as flsFunctions from "./modules/functions.js";
-import Swiper, { Navigation, Pagination } from 'swiper';
+//import Swiper, { Navigation, Pagination } from 'swiper';
 
-const swiper = new Swiper();
+//const swiper = new Swiper();
 
 flsFunctions.testWebP();
 
+const menu = document.querySelector(".burger_menu");
+const menuTopLine = document.querySelector(".line_unique");
+const linesToCross = document.querySelectorAll(".line");
+
+menu.addEventListener("click", function () {
+    if(menu.classList.contains("active")){
+        menu.classList.remove("active");
+        menuTopLine.classList.remove("hidden");
+        linesToCross.forEach(elem => elem.classList.remove("active"));
+    }else{
+        menu.classList.add("active");
+        menuTopLine.classList.add("hidden");
+        linesToCross.forEach(elem => elem.classList.add("active"));
+    }
+})
